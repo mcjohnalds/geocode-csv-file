377a9b5492d7f067f9812cc9a5918310d65173ff
John Owen <johnowen357@gmail.com>
2017-07-08 14:21:25 +1000

    Added gzip compression to HTTP requests

--- src/server/index.js
+++ src/server/index.js
@@ -2,10 +2,12 @@ import 'babel-polyfill';
 import express from 'express';
 import morgan from 'morgan';
 import path from 'path';
+import compression from 'compression';
 import api from './api';
 
 function server() {
   let app = express();
+  app.use(compression())
   if (process.env.NODE_ENV !== 'test') {
     app.use(morgan('dev'));
   }
